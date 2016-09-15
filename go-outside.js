6b9523578aabf8adcf588e5c8f473e597ff4e0a9
vagrant <lauren.dentremont@gmail.com>
2016-09-15 16:17:32 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,14 @@
+var temp = 12;
+
+if (temp < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temp < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you are ready to go outside!");
+
+
+
